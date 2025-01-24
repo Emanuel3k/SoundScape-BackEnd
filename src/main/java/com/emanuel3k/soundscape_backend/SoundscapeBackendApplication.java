38f66f2f0f6ccc00f00e3740394a090850c04a9e
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-01-24 14:55:23 -0300

    feat: start project

---
+++ src/main/java/com/emanuel3k/soundscape_backend/SoundscapeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel3k.soundscape_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundscapeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoundscapeBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/emanuel3k/soundscape_backend/SoundscapeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel3k.soundscape_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundscapeBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
