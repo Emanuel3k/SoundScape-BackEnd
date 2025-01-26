252142970a96266cb320c21108f422f2c4b1464a
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-01-26 16:11:27 -0300

    feat: security and token config

---
+++ src/main/java/com/emanuel3k/soundscape_backend/infra/security/TokenService.java
@@ -0,0 +1,53 @@
+package com.emanuel3k.soundscape_backend.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.emanuel3k.soundscape_backend.domain.user.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+  @Value("${api.security.token.secret}")
+  private String secret;
+
+  public String generateToken(User user) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+
+      return JWT.create()
+              .withIssuer("SoundScape")
+              .withSubject(user.getUsername())
+              .withClaim("role", user.getRole().toString())
+              .withExpiresAt(GenerateExpirationDate())
+              .sign(algorithm);
+
+    } catch (JWTCreationException e) {
+      throw new RuntimeException("Error while generating token");
+    }
+  }
+  
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      
+      return  JWT.require(algorithm)
+              .withIssuer("SoundScape")
+              .build()
+              .verify(token)
+              .getSubject();
+    } catch (Exception e) {
+      throw new RuntimeException("Invalid token");
+    }
+  }
+
+  private Instant GenerateExpirationDate() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+  }
+}
