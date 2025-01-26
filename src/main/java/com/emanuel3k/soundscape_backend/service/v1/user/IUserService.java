bc93f65fbfc51ca70bf230a676f84f00a8267655
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-01-26 16:13:20 -0300

    feat: login user method

--- src/main/java/com/emanuel3k/soundscape_backend/service/v1/user/IUserService.java
+++ src/main/java/com/emanuel3k/soundscape_backend/service/v1/user/IUserService.java
@@ -1,8 +1,11 @@
 package com.emanuel3k.soundscape_backend.service.v1.user;
 
+import com.emanuel3k.soundscape_backend.domain.auth.dto.LoginRequestDTO;
 import com.emanuel3k.soundscape_backend.domain.user.dto.CreateUserDTO;
 import com.emanuel3k.soundscape_backend.domain.user.model.User;
 
 public interface IUserService {
-  User createUser(CreateUserDTO user);
+  User createUser(CreateUserDTO body);
+
+  String loginUser(LoginRequestDTO body);
 }
--- src/main/java/com/emanuel3k/soundscape_backend/service/v1/user/UserService.java
+++ src/main/java/com/emanuel3k/soundscape_backend/service/v1/user/UserService.java
@@ -1,19 +1,62 @@
 package com.emanuel3k.soundscape_backend.service.v1.user;
 
+import com.emanuel3k.soundscape_backend.domain.auth.dto.LoginRequestDTO;
 import com.emanuel3k.soundscape_backend.domain.user.dto.CreateUserDTO;
 import com.emanuel3k.soundscape_backend.domain.user.model.User;
+import com.emanuel3k.soundscape_backend.infra.exception.BadRequestException;
+import com.emanuel3k.soundscape_backend.infra.security.TokenService;
 import com.emanuel3k.soundscape_backend.repository.v1.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
   @Autowired
   UserRepository userRepository;
 
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Autowired
+  TokenService tokenService;
+
   @Override
   public User createUser(CreateUserDTO body) {
 
-    return userRepository.save(body.toUser());
+    User user = body.toUser();
+
+    Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+
+    if (existingUser.isPresent()) {
+      throw new BadRequestException("Username already exists");
+    }
+
+    existingUser = userRepository.findByEmail(user.getEmail());
+
+    if (existingUser.isPresent()) {
+      throw new BadRequestException("Email already exists");
+    }
+
+    user.setPassword(
+            passwordEncoder.encode(
+                    user.getPassword()
+            )
+    );
+    
+    return userRepository.save(user);
+  }
+
+  @Override
+  public String loginUser(LoginRequestDTO body) {
+    User user = userRepository.findByUsername(body.username()).orElseThrow(() -> new BadRequestException("Username or password is incorrect"));
+
+    if (!passwordEncoder.matches(body.password(), user.getPassword())) {
+      throw new BadRequestException("Username or password is incorrect");
+    }
+
+    return tokenService.generateToken(user);
   }
 }
