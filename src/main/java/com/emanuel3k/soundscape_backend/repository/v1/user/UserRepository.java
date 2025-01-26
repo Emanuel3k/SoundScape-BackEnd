db52bc593c364649ae00580b676da508367c7938
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-01-26 16:14:20 -0300

    feat: new methods for get user

--- src/main/java/com/emanuel3k/soundscape_backend/repository/v1/user/UserRepository.java
+++ src/main/java/com/emanuel3k/soundscape_backend/repository/v1/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.emanuel3k.soundscape_backend.repository.v1.user;
 
 import com.emanuel3k.soundscape_backend.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
+  Optional<User> findByUsername(String username);
+  Optional<User> findByEmail(String email);
 }
