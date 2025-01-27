38febb63667dd3ff5ca1f486c2ff96f41a1ac878
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-01-27 20:42:06 -0300

    feat: cache dependency config

--- src/main/java/com/emanuel3k/soundscape_backend/SoundscapeBackendApplication.java
+++ src/main/java/com/emanuel3k/soundscape_backend/SoundscapeBackendApplication.java
@@ -2,8 +2,10 @@ package com.emanuel3k.soundscape_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SoundscapeBackendApplication {
 
   public static void main(String[] args) {
